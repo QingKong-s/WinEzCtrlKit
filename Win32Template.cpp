f1f134f5966366bdb1137168cb90e414216c6add
QingKong <639582106@qq.com>
2024-05-25 15:34:17 +0800

    处理一些警告

--- Win32Template.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "eck/Env.h"
-#include "CTestWnd.h"
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pszCmdLine, _In_ int nCmdShow)
-{
-	return 0;
-}
