5f48f04a6c7a21d57c271866067b536676e32554
qk <85665984+QingKong-s@users.noreply.github.com>
2024-09-24 18:24:46 +0800

    增加ISO8601处理；增加几个简单图像处理操作

---
+++ eck/CImageMat.h
@@ -0,0 +1,422 @@
+/*
+* WinEzCtrlKit Library
+*
+* CImgMat.h : 图像矩阵
+*
+* Copyright(C) 2023-2024 QingKong
+*/
+#pragma once
+#include "CArray.h"
+#include "MathHelper.h"
+
+#include <stack>
+
+ECK_NAMESPACE_BEGIN
+inline constexpr float KernelX_Sobel[]
+{
+	-1,0,1,
+	-2,0,2,
+	-1,0,1
+};
+inline constexpr float KernelY_Sobel[]
+{
+	1,2,1,
+	0,0,0,
+	-1,-2,-1
+};
+
+inline constexpr float KernelX_Prewitt[]
+{
+	1,0,-1,
+	1,0,-1,
+	1,0,-1
+};
+inline constexpr float KernelY_Prewitt[]
+{
+	1,1,1,
+	0,0,0,
+	-1,-1,-1
+};
+
+class CImageMat
+{
+protected:
+	union TArgb
+	{
+		struct
+		{
+			BYTE b, g, r, a;
+		};
+		DWORD dw;
+	};
+
+	using TComp = decltype(TArgb::r);
+	using TColor = decltype(TArgb::dw);
+
+	void* m_pBits{};
+	int m_cx{}, m_cy{};
+	UINT m_cbStride{};
+
+	void IntEdgeDetect(CImageMat& Dst, const float* KernelX, const float* KernelY,
+		int cxyKernel, BOOL bBinary, float fThreshold) const
+	{
+		const int cxyHalf = cxyKernel / 2;
+		for (int i = cxyHalf; i < m_cx - cxyHalf; ++i)
+			for (int j = cxyHalf; j < m_cy - cxyHalf; ++j)
+			{
+				TArgb pix;
+				float fSumX{};
+				float fSumY{};
+				for (int l = -cxyHalf; l <= cxyHalf; ++l)
+					for (int k = -cxyHalf; k <= cxyHalf; ++k)
+					{
+						pix = Pixel(i + k, j + l);
+						const auto fX = KernelX[(k + cxyHalf) + (l + cxyHalf) * 3];
+						fSumX += (fX * pix.r);
+						const auto fY = KernelY[(k + cxyHalf) + (l + cxyHalf) * 3];
+						fSumY += (fY * pix.r);
+					}
+				const auto f = sqrt(fSumX * fSumX + fSumY * fSumY);
+				auto& NewPix = Dst.Pixel(i, j);
+				if (bBinary)
+					NewPix.dw = ((f > fThreshold) ? 0xFFFFFFFF : 0);
+				else
+				{
+					NewPix.r = NewPix.g = NewPix.b = (BYTE)f;
+					NewPix.a = 0xFF;
+				}
+			}
+	}
+public:
+	constexpr CImageMat() = default;
+	constexpr CImageMat(void* pBits, int cx, int cy, UINT cbStride)
+		: m_pBits{ pBits }, m_cx{ cx }, m_cy{ cy }, m_cbStride{ cbStride } {
+	}
+
+	/// <summary>
+	/// 置数据
+	/// </summary>
+	/// <param name="pBits">数据指针</param>
+	/// <param name="cx">宽度</param>
+	/// <param name="cy">高度</param>
+	/// <param name="cbStride">跨步</param>
+	EckInline constexpr void SetBits(void* pBits, int cx, int cy, UINT cbStride)
+	{
+		m_pBits = pBits;
+		m_cx = cx;
+		m_cy = cy;
+		m_cbStride = cbStride;
+	}
+
+	EckInline constexpr int GetWidth() const { return m_cx; }
+
+	EckInline constexpr int GetHeight() const { return m_cy; }
+
+	// 取某点像素
+	EckInline constexpr TArgb& Pixel(int x, int y) const
+	{
+		return *(TArgb*)((BYTE*)m_pBits + y * m_cbStride + x * sizeof(ARGB));
+	}
+
+	/// <summary>
+	/// 卷积
+	/// </summary>
+	/// <param name="Kernel">卷积核</param>
+	/// <param name="cxyKernel">核大小，奇数</param>
+	constexpr void Convolve(CImageMat& Dst, const float* Kernel, int cxyKernel) const
+	{
+		const int cxyHalf = cxyKernel / 2;
+		for (int i = cxyHalf; i < m_cx - cxyHalf; ++i)
+			for (int j = cxyHalf; j < m_cy - cxyHalf; ++j)
+			{
+				TArgb pix;
+				float fSum[4]{};
+				for (int l = -cxyHalf; l <= cxyHalf; ++l)
+					for (int k = -cxyHalf; k <= cxyHalf; ++k)
+					{
+						pix = Pixel(i + k, j + l);
+						const auto f = Kernel[(k + cxyHalf) + (l + cxyHalf) * cxyKernel];
+						fSum[0] += (f * pix.a);
+						fSum[1] += (f * pix.r);
+						fSum[2] += (f * pix.g);
+						fSum[3] += (f * pix.b);
+					}
+				auto& pixNew = Dst.Pixel(i, j);
+				pixNew.a = (BYTE)fSum[0];
+				pixNew.r = (BYTE)fSum[1];
+				pixNew.g = (BYTE)fSum[2];
+				pixNew.b = (BYTE)fSum[3];
+			}
+	}
+
+	EckInline void Sobel(CImageMat& Dst, BOOL bBinary, float fThreshold) const
+	{
+		IntEdgeDetect(Dst, KernelX_Sobel, KernelY_Sobel, 3, bBinary, fThreshold);
+	}
+
+	EckInline void Prewitt(CImageMat& Dst, BOOL bBinary, float fThreshold) const
+	{
+		IntEdgeDetect(Dst, KernelX_Prewitt, KernelY_Prewitt, 3, bBinary, fThreshold);
+	}
+
+	/// <summary>
+	/// Canny边缘检测。
+	/// 此方法不修改当前图像，图像处理前应高斯滤波
+	/// </summary>
+	/// <param name="Dst">检测结果</param>
+	/// <param name="fLowThresh">低阈值</param>
+	/// <param name="fHighThresh">高阈值</param>
+	void Canny(CImageMat& Dst, float fLowThresh, float fHighThresh) const
+	{
+		struct GRADIENT
+		{
+			float g;
+			float fAngle;
+		};
+		CArray<GRADIENT> G{};
+		G.ReDim(m_cx, m_cy);
+		// 计算梯度
+		constexpr int cxyHalf = 3 / 2;
+		for (int i = cxyHalf; i < m_cx - cxyHalf; ++i)
+			for (int j = cxyHalf; j < m_cy - cxyHalf; ++j)
+			{
+				TArgb pix;
+				float fSumX{};
+				float fSumY{};
+				for (int l = -cxyHalf; l <= cxyHalf; ++l)
+					for (int k = -cxyHalf; k <= cxyHalf; ++k)
+					{
+						pix = Pixel(i + k, j + l);
+						const auto fX = KernelX_Sobel[(k + cxyHalf) + (l + cxyHalf) * 3];
+						fSumX += (fX * pix.r);
+						const auto fY = KernelY_Sobel[(k + cxyHalf) + (l + cxyHalf) * 3];
+						fSumY += (fY * pix.r);
+					}
+				auto& e = G[i][j].Data();
+				e.g = sqrt(fSumX * fSumX + fSumY * fSumY);
+				e.fAngle = Rad2Deg(atan2(fSumY, fSumX));
+			}
+
+		CArray<float> Suppressed{};
+		Suppressed.ReDim(m_cx, m_cy);
+		// 非极大值抑制
+		for (int i = cxyHalf; i < m_cx - cxyHalf; ++i)
+			for (int j = cxyHalf; j < m_cy - cxyHalf; ++j)
+			{
+				const auto& e = G[i][j].Data();
+				auto& f = Suppressed[i][j].Data();
+				float angle = (e.fAngle < 0.f ? (e.fAngle + 180.f) : e.fAngle);
+				angle = fmod(angle, 180.f);
+
+				float q, r;
+				if (angle < 22.5 || angle >= 157.5)// 水平
+				{
+					q = G[i + 1][j].Data().g;
+					r = G[i - 1][j].Data().g;
+				}
+				else if (angle >= 22.5 && angle < 67.5)// 45度
+				{
+					q = G[i - 1][j + 1].Data().g;
+					r = G[i + 1][j - 1].Data().g;
+				}
+				else if (angle >= 67.5 && angle < 112.5)// 垂直
+				{
+					q = G[i][j + 1].Data().g;
+					r = G[i][j - 1].Data().g;
+				}
+				else if (angle >= 112.5 && angle < 157.5)// 135度
+				{
+					q = G[i - 1][j - 1].Data().g;
+					r = G[i + 1][j + 1].Data().g;
+				}
+				else
+					q = r = 0.f;
+
+				if (G[i][j].Data().g >= q && G[i][j].Data().g >= r)
+					f = e.g;
+				else
+					f = 0.f;
+			}
+		// 双阈值
+		constexpr TColor WeakColor{ 0xFFFF0000 };
+		constexpr TColor WeakLabelColor{ 0xFF00FF00 };
+		for (int i = cxyHalf; i < m_cx - cxyHalf; ++i)
+			for (int j = cxyHalf; j < m_cy - cxyHalf; ++j)
+			{
+				const auto f = Suppressed[i][j].Data();
+				auto& Pix = Dst.Pixel(i, j);
+				if (f >= fHighThresh)
+					Pix.dw = 0xFFFFFFFF;
+				else if (f >= fLowThresh)
+					Pix.dw = WeakColor;
+				else
+					Pix.dw = 0;
+			}
+		// 弱边处理
+		std::stack<POINT> s{};
+		std::queue<POINT> q{};
+		BOOL bConnected = FALSE;
+		for (int i = cxyHalf; i < m_cx - cxyHalf; ++i)
+			for (int j = cxyHalf; j < m_cy - cxyHalf; ++j)
+			{
+				auto& Pix = Dst.Pixel(i, j);
+				if (Pix.dw == WeakColor)
+				{
+					s.emplace(i, j);
+					q.emplace(i, j);
+					Pix.dw = WeakLabelColor;
+
+					while (!s.empty())
+					{
+						const POINT pt = s.top();
+						s.pop();
+						auto& PixCurr = Dst.Pixel(pt.x, pt.y);
+						for (int k = -1; k <= 1; ++k)
+							for (int l = -1; l <= 1; ++l)
+							{
+								auto& PixNeighbor = Dst.Pixel(pt.x + k, pt.y + l);
+								if (PixNeighbor.dw == WeakColor)// 弱点
+								{
+									s.emplace(pt.x + k, pt.y + l);
+									q.emplace(pt.x + k, pt.y + l);
+									PixNeighbor.dw = WeakLabelColor;
+								}
+								if (bConnected == FALSE && PixNeighbor.dw == 0xFFFFFFFF)// 强点
+									bConnected = TRUE;
+							}
+					}
+
+					if (bConnected == FALSE)
+					{
+						while (!q.empty())
+						{
+							const POINT pt = q.front();
+							q.pop();
+							Dst.Pixel(pt.x, pt.y).dw = 0;
+						}
+					}
+					else
+					{
+						while (!q.empty())
+						{
+							const POINT pt = q.front();
+							q.pop();
+							Dst.Pixel(pt.x, pt.y).dw = 0xFFFFFFFF;
+						}
+						bConnected = FALSE;
+					}
+				}
+			}
+	}
+};
+
+class CGpImageMat : public CImageMat
+{
+private:
+	GpBitmap* m_pBitmap{};
+	Gdiplus::BitmapData m_BitmapData{};
+public:
+	ECK_DISABLE_COPY_DEF_CONS(CGpImageMat);
+	CGpImageMat(int cx, int cy)
+	{
+		GdipCreateBitmapFromScan0(cx, cy, 0, PixelFormat32bppARGB, nullptr, &m_pBitmap);
+		Lock();
+		SetBits(m_BitmapData.Scan0, m_BitmapData.Width, m_BitmapData.Height, m_BitmapData.Stride);
+	}
+
+	CGpImageMat(GpBitmap* pBitmap) : m_pBitmap{ pBitmap }
+	{
+		Lock();
+		SetBits(m_BitmapData.Scan0, m_BitmapData.Width, m_BitmapData.Height, m_BitmapData.Stride);
+	}
+
+	CGpImageMat(CGpImageMat&& x) noexcept
+		: m_pBitmap{ x.m_pBitmap }, m_BitmapData{ x.m_BitmapData }
+	{
+		x.m_pBitmap = nullptr;
+		x.m_BitmapData = {};
+		m_pBits = x.m_pBits;
+		m_cx = x.m_cx;
+		m_cy = x.m_cy;
+		m_cbStride = x.m_cbStride;
+	}
+
+	CGpImageMat& operator=(CGpImageMat&& x) noexcept
+	{
+		if (this != &x)
+		{
+			Unlock();
+			GdipDisposeImage(m_pBitmap);
+			m_pBitmap = x.m_pBitmap;
+			m_BitmapData = x.m_BitmapData;
+			x.m_pBitmap = nullptr;
+			x.m_BitmapData = {};
+			m_pBits = x.m_pBits;
+			m_cx = x.m_cx;
+			m_cy = x.m_cy;
+			m_cbStride = x.m_cbStride;
+		}
+		return *this;
+	}
+
+	~CGpImageMat()
+	{
+		Unlock();
+		GdipDisposeImage(m_pBitmap);
+	}
+
+	EckInline constexpr GpBitmap* GetGpBitmap() const { return m_pBitmap; }
+
+	EckInline constexpr const Gdiplus::BitmapData& GetBitmapData() { return m_BitmapData; }
+
+	EckInline constexpr BOOL IsValid() const { return m_pBitmap != nullptr; }
+
+	EckInline GpStatus Lock()
+	{
+		if (!m_BitmapData.Scan0)
+			return GdipBitmapLockBits(m_pBitmap, nullptr, Gdiplus::ImageLockModeRead | Gdiplus::ImageLockModeWrite,
+				PixelFormat32bppARGB, &m_BitmapData);
+		else
+			return GpStatus::WrongState;
+	}
+
+	EckInline GpStatus Unlock()
+	{
+		if (m_BitmapData.Scan0)
+		{
+			const auto r = GdipBitmapUnlockBits(m_pBitmap, &m_BitmapData);
+			m_BitmapData = {};
+			return r;
+		}
+		else
+			return GpStatus::WrongState;
+	}
+
+	EckInline GpBitmap* Attach(GpBitmap* pBitmap)
+	{
+		Unlock();
+		const auto pOld = m_pBitmap;
+		m_pBitmap = pBitmap;
+		Lock();
+		return pOld;
+	}
+
+	EckInline GpBitmap* Detach()
+	{
+		Unlock();
+		m_BitmapData = {};
+		return std::exchange(m_pBitmap, nullptr);
+	}
+
+	EckInline [[nodiscard]] CGpImageMat Duplicate()
+	{
+		Unlock();
+		GpBitmap* pClone = nullptr;
+		GdipCloneBitmapAreaI(0, 0, m_BitmapData.Width, m_BitmapData.Height,
+			PixelFormat32bppARGB, m_pBitmap, &pClone);
+		CGpImageMat Ret{ pClone };
+		Lock();
+		return Ret;
+	}
+};
+ECK_NAMESPACE_END
---
+++ eck/ISO8601Time.h
@@ -0,0 +1,412 @@
+﻿/*
+* WinEzCtrlKit Library
+*
+* ISO8601Time.h : ISO8601处理
+*
+* Copyright(C) 2024 QingKong
+*/
+#pragma once
+#include "DateTimeHelper.h"
+#include "CRefStr.h"
+
+ECK_NAMESPACE_BEGIN
+inline BOOL Iso8601ToSystemTime(PCSTR pszIso8601, SYSTEMTIME& st)
+{
+	// 自动机状态
+	enum class State
+	{
+		Year,
+		AfterYear,
+		Month,
+		Day,
+		Hour,
+		Minute,
+		Second,
+		Millisecond,
+		Accept,
+	};
+
+	// 时区调节
+	enum class Timezone
+	{
+		None,
+		UTC,
+		Plus,
+		Minus,
+	};
+
+	// 跟在年份之后的字段
+	enum class AfterYear
+	{
+		None,
+		Month,
+		Week,
+		NumOfDays,
+	};
+
+	AfterYear eAfterYear = AfterYear::None;
+	Timezone eTimezone = Timezone::None;
+
+	State eState = State::Year;
+	auto p = pszIso8601;
+	auto pStateBegin = pszIso8601;
+	if (*p == '+' || *p == '-')
+		++p;
+
+	EckLoop()
+	{
+		const auto ch = *p;
+		switch (eState)
+		{
+		case State::Year:
+		{
+			if (ch >= '0' && ch <= '9')
+				++p;
+			else if (ch == '-')
+			{
+				eState = State::AfterYear;
+				++p;
+				if (*p == 'W')
+				{
+					++p;
+					eAfterYear = AfterYear::Week;
+				}
+			}
+			else if (ch == 'Z' || ch == '+' || ch == '-')
+			{
+				if (eTimezone != Timezone::None)
+					return FALSE;
+				eState = State::Hour;
+				switch (ch)
+				{
+				case 'Z':eTimezone = Timezone::UTC; break;
+				case '+':eTimezone = Timezone::Plus; break;
+				case '-':eTimezone = Timezone::Minus; break;
+				default:ECK_UNREACHABLE;
+				}
+				++p;
+			}
+			else if (ch == '\0')
+			{
+				eState = State::Accept;
+				++p;
+			}
+			else
+				return FALSE;
+			if (eState != State::Year)
+			{
+				st.wYear = (WORD)atoi(pStateBegin);
+				pStateBegin = p;
+			}
+		}
+		break;
+		case State::AfterYear:// 可接月、周、顺序日
+		{
+			if (ch >= '0' && ch <= '9')
+				++p;
+			else if (ch == '-')
+			{
+				if (eAfterYear != AfterYear::Week)
+				{
+					if (eAfterYear != AfterYear::None)
+						return FALSE;
+					eAfterYear = AfterYear::Month;
+				}
+				eState = State::Day;
+				++p;
+			}
+			else if (ch == 'Z' || ch == '+' || ch == '-')
+			{
+				if (eTimezone != Timezone::None)
+					return FALSE;
+				eState = State::Hour;
+				switch (ch)
+				{
+				case 'Z':eTimezone = Timezone::UTC; break;
+				case '+':eTimezone = Timezone::Plus; break;
+				case '-':eTimezone = Timezone::Minus; break;
+				default:ECK_UNREACHABLE;
+				}
+				++p;
+			}
+			else if (ch == '\0')
+			{
+				eState = State::Accept;
+				++p;
+			}
+			else
+				return FALSE;
+			if (eState != State::AfterYear)
+			{
+				const int cch = (int)(p - pStateBegin);
+				if (cch == 4)// 顺序日
+				{
+					const auto t = atoi(pStateBegin);
+					if (t < 1 || t > 366)
+						return FALSE;
+					st.wDay = (WORD)t;
+					GetNumOfDaysDate(st);
+				}
+				else if (cch == 3)// 周、月
+				{
+					if (eAfterYear == AfterYear::Week)
+					{
+						if (eState != State::Day)
+						{
+							st.wDay = ((WORD)atoi(pStateBegin) - 1) * 7;
+							if (st.wDay < 1 || st.wDay > 366)
+								return FALSE;
+							GetNumOfDaysDate(st);
+						}
+						else
+							st.wDayOfWeek = (WORD)atoi(pStateBegin);
+					}
+					else
+						st.wMonth = (WORD)atoi(pStateBegin);
+					pStateBegin = p;
+				}
+				else
+					return FALSE;
+			}
+		}
+		break;
+		case State::Day:
+		{
+			if (ch >= '0' && ch <= '9')
+				++p;
+			else if (ch == 'T')
+			{
+				eState = State::Hour;
+				++p;
+			}
+			else if (ch == 'Z' || ch == '+' || ch == '-')
+			{
+				if (eTimezone != Timezone::None)
+					return FALSE;
+				eState = State::Hour;
+				switch (ch)
+				{
+				case 'Z':eTimezone = Timezone::UTC; break;
+				case '+':eTimezone = Timezone::Plus; break;
+				case '-':eTimezone = Timezone::Minus; break;
+				default:ECK_UNREACHABLE;
+				}
+				++p;
+			}
+			else if (ch == '\0')
+			{
+				eState = State::Accept;
+				++p;
+			}
+			else
+				return FALSE;
+			if (eState != State::Day)
+			{
+				if (st.wDayOfWeek)
+				{
+					st.wDay = (st.wDayOfWeek - 1) * 7 + (WORD)atoi(pStateBegin);
+					if (st.wDay < 1 || st.wDay > 366)
+						return FALSE;
+					st.wDayOfWeek = 0;
+					GetNumOfDaysDate(st);
+				}
+				else
+					st.wDay = (WORD)atoi(pStateBegin);
+				pStateBegin = p;
+			}
+		}
+		break;
+		case State::Hour:
+		{
+			BOOL bConvertToTimezones = FALSE;
+			if (ch >= '0' && ch <= '9')
+				++p;
+			else if (ch == ':')
+			{
+				eState = State::Minute;
+				++p;
+			}
+			else if (ch == 'Z' || ch == '+' || ch == '-')
+			{
+				if (eTimezone != Timezone::None)
+					return FALSE;
+				eState = State::Hour;
+				bConvertToTimezones = TRUE;
+				switch (ch)
+				{
+				case 'Z':eTimezone = Timezone::UTC; break;
+				case '+':eTimezone = Timezone::Plus; break;
+				case '-':eTimezone = Timezone::Minus; break;
+				default:ECK_UNREACHABLE;
+				}
+				++p;
+			}
+			else if (ch == '\0')
+			{
+				eState = State::Accept;
+				++p;
+			}
+			else
+				return FALSE;
+			if (eState != State::Hour || bConvertToTimezones)
+			{
+				if (eTimezone == Timezone::Plus)
+					TimeDeltaHour(st, atoi(pStateBegin));
+				else if (eTimezone == Timezone::Minus)
+					TimeDeltaHour(st, -atoi(pStateBegin));
+				else
+					st.wHour = (WORD)atoi(pStateBegin);
+				pStateBegin = p;
+			}
+		}
+		break;
+		case State::Minute:
+		{
+			if (ch >= '0' && ch <= '9')
+				++p;
+			else if (ch == ':')
+			{
+				eState = State::Second;
+				++p;
+			}
+			else if (ch == 'Z' || ch == '+' || ch == '-')
+			{
+				if (eTimezone != Timezone::None)
+					return FALSE;
+				eState = State::Hour;
+				switch (ch)
+				{
+				case 'Z':eTimezone = Timezone::UTC; break;
+				case '+':eTimezone = Timezone::Plus; break;
+				case '-':eTimezone = Timezone::Minus; break;
+				default:ECK_UNREACHABLE;
+				}
+				++p;
+			}
+			else if (ch == '\0')
+			{
+				eState = State::Accept;
+				++p;
+			}
+			else
+				return FALSE;
+			if (eState != State::Minute)
+			{
+				if (eTimezone != Timezone::None && eState != State::Accept)
+					return FALSE;
+				if (eTimezone == Timezone::Plus)
+					TimeDeltaMinute(st, atoi(pStateBegin));
+				else if (eTimezone == Timezone::Minus)
+					TimeDeltaMinute(st, -atoi(pStateBegin));
+				else
+					st.wMinute = (WORD)atoi(pStateBegin);
+				pStateBegin = p;
+			}
+		}
+		break;
+		case State::Second:
+		{
+			if (ch >= '0' && ch <= '9')
+				++p;
+			else if (ch == '.')
+			{
+				eState = State::Millisecond;
+				++p;
+			}
+			else if (ch == 'Z' || ch == '+' || ch == '-')
+			{
+				if (eTimezone != Timezone::None)
+					return FALSE;
+				eState = State::Hour;
+				switch (ch)
+				{
+				case 'Z':eTimezone = Timezone::UTC; break;
+				case '+':eTimezone = Timezone::Plus; break;
+				case '-':eTimezone = Timezone::Minus; break;
+				default:ECK_UNREACHABLE;
+				}
+				++p;
+			}
+			else if (ch == '\0')
+			{
+				eState = State::Accept;
+				++p;
+			}
+			else
+				return FALSE;
+			if (eState != State::Second)
+			{
+				st.wSecond = (WORD)atoi(pStateBegin);
+				pStateBegin = p;
+			}
+		}
+		break;
+		case State::Millisecond:
+		{
+			if (ch >= '0' && ch <= '9')
+				++p;
+			else if (ch == 'Z' || ch == '+' || ch == '-')
+			{
+				if (eTimezone != Timezone::None)
+					return FALSE;
+				eState = State::Hour;
+				switch (ch)
+				{
+				case 'Z':eState = State::Accept; break;
+				case '+':eTimezone = Timezone::Plus; break;
+				case '-':eTimezone = Timezone::Minus; break;
+				default:ECK_UNREACHABLE;
+				}
+				++p;
+			}
+			else if (ch == '\0')
+			{
+				eState = State::Accept;
+				++p;
+			}
+			else
+				return FALSE;
+			if (eState != State::Millisecond)
+			{
+				st.wMilliseconds = (WORD)atoi(pStateBegin);
+				pStateBegin = p;
+			}
+		}
+		break;
+		case State::Accept: return TRUE;
+		default: ECK_UNREACHABLE;
+		}
+	}
+	return FALSE;
+}
+
+inline void SystemTimeToIso8601(const SYSTEMTIME& st, CRefStrA& rs, int iTzHour = 0, int iTzMinute = 0)
+{
+	rs.AppendFormat("%04d", st.wYear);
+	if (st.wMonth)
+		rs.AppendFormat("-%02d", st.wMonth);
+	if (st.wDay)
+		rs.AppendFormat("-%02d", st.wDay);
+	if (st.wHour || st.wMinute || st.wSecond || st.wMilliseconds)
+	{
+		rs.AppendFormat("T%02d", st.wHour);
+		if (st.wMinute)
+			rs.AppendFormat(":%02d", st.wMinute);
+		if (st.wSecond)
+			rs.AppendFormat(":%02d", st.wSecond);
+		if (st.wMilliseconds)
+			rs.AppendFormat(".%03d", st.wMilliseconds);
+	}
+	if (iTzHour == INT_MIN)
+		rs.PushBackChar('Z');
+	else if (iTzHour)
+	{
+		if (iTzHour > 0)
+			rs.AppendFormat("+%02d", iTzHour);
+		else
+			rs.AppendFormat("-%02d", -iTzHour);
+		if (iTzMinute)
+			rs.AppendFormat(":%02d", iTzMinute);
+	}
+}
+ECK_NAMESPACE_END
