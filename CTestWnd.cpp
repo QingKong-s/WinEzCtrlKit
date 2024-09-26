6b5048d14cd942224ad32b262bd5e9f14b65c01a
QingKong <639582106@qq.com>
2024-09-26 11:59:23 +0800

    1

---
+++ CTestWnd.cpp
@@ -0,0 +1,14 @@
+﻿#include "CTestWnd.h"
+
+void CTestWnd::Test()
+{
+	using namespace eck;
+	using namespace eck::Literals;
+	
+}
+
+LRESULT CTestWnd::OnMsg(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	
+	return __super::OnMsg(hWnd, uMsg, wParam, lParam);
+}
---
+++ CTestWnd.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "eck\CForm.h"
+
+using namespace eck;
+
+struct CTestWnd :public eck::CForm
+{
+	void Test();
+
+	BOOL PreTranslateMessage(const MSG& Msg) override
+	{
+		return CForm::PreTranslateMessage(Msg);
+	}
+
+	LRESULT OnMsg(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) override;
+
+	ECK_CWND_CREATE;
+	HWND Create(PCWSTR pszText, DWORD dwStyle, DWORD dwExStyle,
+		int x, int y, int cx, int cy, HWND hParent, HMENU hMenu, PCVOID pData = nullptr) override
+	{
+		IntCreate(dwExStyle, WCN_BK, pszText, dwStyle | WS_CLIPCHILDREN,
+			x, y, cx, cy, hParent, hMenu, g_hInstance, this);
+		EckDbgPrintWndMap();
+		return m_hWnd;
+	}
+};
