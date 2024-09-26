3e5e7f9f3a1a33fac904e3e394171254cfe09bf8
qk <85665984+QingKong-s@users.noreply.github.com>
2024-09-26 11:59:23 +0800

    1

---
+++ t_main.cpp
@@ -0,0 +1,34 @@
+#include "CTestWnd.h"
+
+#include "eck/Env.h"
+
+using namespace std::literals;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pszCmdLine, _In_ int nCmdShow)
+{
+	OleInitialize(NULL);
+	if (eck::InitStatus b; (b = eck::Init(hInstance)) != eck::InitStatus::Ok)
+	{
+		MessageBoxW(NULL, L"初始化失败", std::to_wstring((int)b).c_str(), 0);
+		return 1;
+	}
+
+	using namespace eck;
+	using namespace eck::Literals;
+
+	MSG msg;
+	
+	CTestWnd w;
+	w.Create(L"示例Win32程序", WS_OVERLAPPEDWINDOW, 0, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, 0);
+	w.Show(SW_SHOW);
+
+	while (GetMessageW(&msg, NULL, 0, 0))
+	{
+		if (!eck::PreTranslateMessage(msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+		}
+	}
+	return (int)msg.wParam;
+}
