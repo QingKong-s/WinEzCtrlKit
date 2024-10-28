93c18cd8347f3ccb6d2cfcea293f97699d190071
qk <85665984+QingKong-s@users.noreply.github.com>
2024-10-28 15:51:36 +0800

    修复渐变填充忽略矩形坐标的问题；整理Label

--- eck/PrivateApi.h
+++ eck/PrivateApi.h
@@ -291,122 +291,6 @@ constexpr inline GUID CLSID_CRatingControl{ 0x85e94d25, 0x0712, 0x47ed, {0x8C, 0
 // {527c9a9b-b9a2-44b0-84f9-f0dc11c2bcfb}
 constexpr inline GUID CLSID_CStaticPropertyControl{ 0x527C9A9B, 0xB9A2, 0x44B0, {0x84, 0xF9, 0xF0, 0xDC, 0x11, 0xC2, 0xBC, 0xFB} };
 
-class ISubItemCallback :public IUnknown
-{
-public:
-	/// \brief <em>Retrieves the title of the specified sub-item</em>
-	///
-	/// Retrieves the title of the specified sub-item. This title is displayed in extended tile view mode
-	/// in front of the sub-item's value.
-	///
-	/// \param[in] subItemIndex The one-based index of the sub-item for which to retrieve the title.
-	/// \param[out] pBuffer Receives the sub-item's title.
-	/// \param[in] bufferSize The size of the buffer (in characters) specified by the \c pBuffer parameter.
-	///
-	/// \return An \c HRESULT error code.
-	virtual HRESULT STDMETHODCALLTYPE GetSubItemTitle(int subItemIndex, LPWSTR pBuffer, int bufferSize) = 0;
-	/// \brief <em>Retrieves the control representing the specified sub-item</em>
-	///
-	/// Retrieves the control representing the specified sub-item. The control is used for drawing the
-	/// sub-item.\n
-	/// Starting with comctl32.dll version 6.10, sub-items can be represented by objects that implement
-	/// the \c IPropertyControlBase interface. Representation means drawing the sub-item (by implementing the
-	/// \c IDrawPropertyControl interface) and/or editing the sub-item in-place (by implementing the
-	/// \c IPropertyControl interface, which allows in-place editing with a complex user interface). The
-	/// object that represents the sub-item is retrieved dynamically.\n
-	/// This method retrieves the sub-item control that is used for drawing the sub-item.
-	///
-	/// \param[in] itemIndex The zero-based index of the item for which to retrieve the sub-item control.
-	/// \param[in] subItemIndex The one-based index of the sub-item for which to retrieve the sub-item
-	///            control.
-	/// \param[in] requiredInterface The IID of the interface of which the sub-item control's implementation
-	///            will be returned.
-	/// \param[out] ppObject Receives the sub-item control's implementation of the interface identified by
-	///             \c requiredInterface.
-	///
-	/// \return An \c HRESULT error code.
-	///
-	/// \remarks With current versions of comctl32.dll, providing a sub-item control is the only way to
-	///          custom-draw sub-items in Tiles view mode.
-	///
-	/// \sa BeginSubItemEdit, IPropertyControlBase, IPropertyControl, IDrawPropertyControl
-	virtual HRESULT STDMETHODCALLTYPE GetSubItemControl(int itemIndex, int subItemIndex, REFIID requiredInterface, LPVOID* ppObject) = 0;
-	/// \brief <em>Retrieves the control used to edit the specified sub-item</em>
-	///
-	/// Retrieves the control that will be used to edit the specified sub-item. The control is used for
-	/// editing the sub-item.\n
-	/// Starting with comctl32.dll version 6.10, sub-items can be represented by objects that implement
-	/// the \c IPropertyControlBase interface. Representation means drawing the sub-item (by implementing the
-	/// \c IDrawPropertyControl interface) and/or editing the sub-item in-place (by implementing the
-	/// \c IPropertyControl interface, which allows in-place editing with a complex user interface). The
-	/// object that represents the sub-item is retrieved dynamically.\n
-	/// This method retrieves the sub-item control that is used for editing the sub-item.
-	///
-	/// \param[in] itemIndex The zero-based index of the item for which to retrieve the sub-item control.
-	/// \param[in] subItemIndex The one-based index of the sub-item for which to retrieve the sub-item
-	///            control.
-	/// \param[in] mode If set to 0, the edit mode has been entered by moving the mouse over the sub-item.
-	///            If set to 1, the edit mode has been entered by calling \c IListView::EditSubItem or by
-	///            clicking on the sub-item.
-	/// \param[in] requiredInterface The IID of the interface of which the sub-item control's implementation
-	///            will be returned.
-	/// \param[out] ppObject Receives the sub-item control's implementation of the interface identified by
-	///             \c requiredInterface.
-	///
-	/// \return An \c HRESULT error code.
-	///
-	/// \sa EndSubItemEdit, GetSubItemControl, IPropertyControlBase,
-	///     IPropertyControl, IDrawPropertyControl
-	virtual HRESULT STDMETHODCALLTYPE BeginSubItemEdit(int itemIndex, int subItemIndex, int mode, REFIID requiredInterface, LPVOID* ppObject) = 0;
-	/// \brief <em>Notifies the control that editing the specified sub-item has ended</em>
-	///
-	/// Notifies the control that editing the specified sub-item using the specified sub-item control has
-	/// been finished.\n
-	/// Starting with comctl32.dll version 6.10, sub-items can be represented by objects that implement
-	/// the \c IPropertyControlBase interface. Representation means drawing the sub-item (by implementing the
-	/// \c IDrawPropertyControl interface) and/or editing the sub-item in-place (by implementing the
-	/// \c IPropertyControl interface, which allows in-place editing with a complex user interface). The
-	/// object that represents the sub-item is retrieved dynamically.
-	///
-	/// \param[in] itemIndex The zero-based index of the item whose sub-item has been edited.
-	/// \param[in] subItemIndex The one-based index of the sub-item that has been edited.
-	/// \param[in] mode If set to 0, the edit mode has been entered by moving the mouse over the sub-item.
-	///            If set to 1, the edit mode has been entered by calling \c IListView::EditSubItem or by
-	///            clicking on the sub-item.
-	/// \param[in] pPropertyControl The property control that has been used to edit the sub-item. This
-	///            property control has to be destroyed by this method.
-	///
-	/// \return An \c HRESULT error code.
-	///
-	/// \remarks Call \c IPropertyControl::IsModified to retrieve whether editing the sub-item has been
-	///          completed or canceled.
-	///
-	/// \sa BeginSubItemEdit, IListView_WINVISTA::EditSubItem, IListView_WIN7::EditSubItem,
-	///     IPropertyControlBase, IPropertyControl, IDrawPropertyControl, IPropertyControlBase::Destroy,
-	///     IPropertyControl::IsModified
-	virtual HRESULT STDMETHODCALLTYPE EndSubItemEdit(int itemIndex, int subItemIndex, int mode, IPropertyControl* pPropertyControl) = 0;
-	// TBD
-	virtual HRESULT STDMETHODCALLTYPE BeginGroupEdit(int groupIndex, REFIID requiredInterface, LPVOID* ppObject) = 0;
-	// TBD
-	virtual HRESULT STDMETHODCALLTYPE EndGroupEdit(int groupIndex, int mode, IPropertyControl* pPropertyControl) = 0;
-	/// \brief <em>Notifies the control that the specified verb has been invoked on the specified item</em>
-	///
-	/// Notifies the control that an action identified by the specified verb has been invoked on the
-	/// specified item. The action usually is generated by the user. The sub-item control translates the
-	/// action into a verb which it invokes.\n
-	/// For the hyperlink sub-item control the action is clicking the link and the verb is the string that
-	/// has been specified as the \c id attribute of the hyperlink markup
-	/// (&lt;a id=&quot;<i>verb</i>&quot;&gt;<i>text</i>&lt;/a&gt;).
-	///
-	/// \param[in] itemIndex The zero-based index of the item on which the verb is being invoked.
-	/// \param[in] pVerb The verb identifying the action.
-	///
-	/// \return An \c HRESULT error code.
-	///
-	/// \sa GetSubItemControl, IPropertyControlBase::InvokeDefaultAction
-	virtual HRESULT STDMETHODCALLTYPE OnInvokeVerb(int itemIndex, LPCWSTR pVerb) = 0;
-};
-
 class IPropertyValue :public IUnknown
 {
 public:
@@ -594,6 +478,122 @@ public:
 	virtual HRESULT STDMETHODCALLTYPE SetModified(BOOL modified) = 0;
 	virtual HRESULT STDMETHODCALLTYPE ValidationFailed(LPCWSTR) = 0;
 	virtual HRESULT STDMETHODCALLTYPE GetState(PINT pState) = 0;
+};
+
+class ISubItemCallback :public IUnknown
+{
+public:
+	/// \brief <em>Retrieves the title of the specified sub-item</em>
+	///
+	/// Retrieves the title of the specified sub-item. This title is displayed in extended tile view mode
+	/// in front of the sub-item's value.
+	///
+	/// \param[in] subItemIndex The one-based index of the sub-item for which to retrieve the title.
+	/// \param[out] pBuffer Receives the sub-item's title.
+	/// \param[in] bufferSize The size of the buffer (in characters) specified by the \c pBuffer parameter.
+	///
+	/// \return An \c HRESULT error code.
+	virtual HRESULT STDMETHODCALLTYPE GetSubItemTitle(int subItemIndex, LPWSTR pBuffer, int bufferSize) = 0;
+	/// \brief <em>Retrieves the control representing the specified sub-item</em>
+	///
+	/// Retrieves the control representing the specified sub-item. The control is used for drawing the
+	/// sub-item.\n
+	/// Starting with comctl32.dll version 6.10, sub-items can be represented by objects that implement
+	/// the \c IPropertyControlBase interface. Representation means drawing the sub-item (by implementing the
+	/// \c IDrawPropertyControl interface) and/or editing the sub-item in-place (by implementing the
+	/// \c IPropertyControl interface, which allows in-place editing with a complex user interface). The
+	/// object that represents the sub-item is retrieved dynamically.\n
+	/// This method retrieves the sub-item control that is used for drawing the sub-item.
+	///
+	/// \param[in] itemIndex The zero-based index of the item for which to retrieve the sub-item control.
+	/// \param[in] subItemIndex The one-based index of the sub-item for which to retrieve the sub-item
+	///            control.
+	/// \param[in] requiredInterface The IID of the interface of which the sub-item control's implementation
+	///            will be returned.
+	/// \param[out] ppObject Receives the sub-item control's implementation of the interface identified by
+	///             \c requiredInterface.
+	///
+	/// \return An \c HRESULT error code.
+	///
+	/// \remarks With current versions of comctl32.dll, providing a sub-item control is the only way to
+	///          custom-draw sub-items in Tiles view mode.
+	///
+	/// \sa BeginSubItemEdit, IPropertyControlBase, IPropertyControl, IDrawPropertyControl
+	virtual HRESULT STDMETHODCALLTYPE GetSubItemControl(int itemIndex, int subItemIndex, REFIID requiredInterface, LPVOID* ppObject) = 0;
+	/// \brief <em>Retrieves the control used to edit the specified sub-item</em>
+	///
+	/// Retrieves the control that will be used to edit the specified sub-item. The control is used for
+	/// editing the sub-item.\n
+	/// Starting with comctl32.dll version 6.10, sub-items can be represented by objects that implement
+	/// the \c IPropertyControlBase interface. Representation means drawing the sub-item (by implementing the
+	/// \c IDrawPropertyControl interface) and/or editing the sub-item in-place (by implementing the
+	/// \c IPropertyControl interface, which allows in-place editing with a complex user interface). The
+	/// object that represents the sub-item is retrieved dynamically.\n
+	/// This method retrieves the sub-item control that is used for editing the sub-item.
+	///
+	/// \param[in] itemIndex The zero-based index of the item for which to retrieve the sub-item control.
+	/// \param[in] subItemIndex The one-based index of the sub-item for which to retrieve the sub-item
+	///            control.
+	/// \param[in] mode If set to 0, the edit mode has been entered by moving the mouse over the sub-item.
+	///            If set to 1, the edit mode has been entered by calling \c IListView::EditSubItem or by
+	///            clicking on the sub-item.
+	/// \param[in] requiredInterface The IID of the interface of which the sub-item control's implementation
+	///            will be returned.
+	/// \param[out] ppObject Receives the sub-item control's implementation of the interface identified by
+	///             \c requiredInterface.
+	///
+	/// \return An \c HRESULT error code.
+	///
+	/// \sa EndSubItemEdit, GetSubItemControl, IPropertyControlBase,
+	///     IPropertyControl, IDrawPropertyControl
+	virtual HRESULT STDMETHODCALLTYPE BeginSubItemEdit(int itemIndex, int subItemIndex, int mode, REFIID requiredInterface, LPVOID* ppObject) = 0;
+	/// \brief <em>Notifies the control that editing the specified sub-item has ended</em>
+	///
+	/// Notifies the control that editing the specified sub-item using the specified sub-item control has
+	/// been finished.\n
+	/// Starting with comctl32.dll version 6.10, sub-items can be represented by objects that implement
+	/// the \c IPropertyControlBase interface. Representation means drawing the sub-item (by implementing the
+	/// \c IDrawPropertyControl interface) and/or editing the sub-item in-place (by implementing the
+	/// \c IPropertyControl interface, which allows in-place editing with a complex user interface). The
+	/// object that represents the sub-item is retrieved dynamically.
+	///
+	/// \param[in] itemIndex The zero-based index of the item whose sub-item has been edited.
+	/// \param[in] subItemIndex The one-based index of the sub-item that has been edited.
+	/// \param[in] mode If set to 0, the edit mode has been entered by moving the mouse over the sub-item.
+	///            If set to 1, the edit mode has been entered by calling \c IListView::EditSubItem or by
+	///            clicking on the sub-item.
+	/// \param[in] pPropertyControl The property control that has been used to edit the sub-item. This
+	///            property control has to be destroyed by this method.
+	///
+	/// \return An \c HRESULT error code.
+	///
+	/// \remarks Call \c IPropertyControl::IsModified to retrieve whether editing the sub-item has been
+	///          completed or canceled.
+	///
+	/// \sa BeginSubItemEdit, IListView_WINVISTA::EditSubItem, IListView_WIN7::EditSubItem,
+	///     IPropertyControlBase, IPropertyControl, IDrawPropertyControl, IPropertyControlBase::Destroy,
+	///     IPropertyControl::IsModified
+	virtual HRESULT STDMETHODCALLTYPE EndSubItemEdit(int itemIndex, int subItemIndex, int mode, IPropertyControl* pPropertyControl) = 0;
+	// TBD
+	virtual HRESULT STDMETHODCALLTYPE BeginGroupEdit(int groupIndex, REFIID requiredInterface, LPVOID* ppObject) = 0;
+	// TBD
+	virtual HRESULT STDMETHODCALLTYPE EndGroupEdit(int groupIndex, int mode, IPropertyControl* pPropertyControl) = 0;
+	/// \brief <em>Notifies the control that the specified verb has been invoked on the specified item</em>
+	///
+	/// Notifies the control that an action identified by the specified verb has been invoked on the
+	/// specified item. The action usually is generated by the user. The sub-item control translates the
+	/// action into a verb which it invokes.\n
+	/// For the hyperlink sub-item control the action is clicking the link and the verb is the string that
+	/// has been specified as the \c id attribute of the hyperlink markup
+	/// (&lt;a id=&quot;<i>verb</i>&quot;&gt;<i>text</i>&lt;/a&gt;).
+	///
+	/// \param[in] itemIndex The zero-based index of the item on which the verb is being invoked.
+	/// \param[in] pVerb The verb identifying the action.
+	///
+	/// \return An \c HRESULT error code.
+	///
+	/// \sa GetSubItemControl, IPropertyControlBase::InvokeDefaultAction
+	virtual HRESULT STDMETHODCALLTYPE OnInvokeVerb(int itemIndex, LPCWSTR pVerb) = 0;
 };
 
 class IDrawPropertyControl :public IPropertyControlBase
