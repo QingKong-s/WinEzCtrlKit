6ba87071ae40448e6a4afbf89240998cae87c162
qk <85665984+QingKong-s@users.noreply.github.com>
2025-01-11 19:48:51 +0800

    增加线程池；增加一些协程辅助设施

---
+++ eck/CoroutineHelper.h
@@ -0,0 +1,349 @@
+﻿#pragma once
+#include "CSrwLock.h"
+#include "ThreadPool.h"
+
+#include <coroutine>
+
+ECK_NAMESPACE_BEGIN
+struct CExceptionCoroCancelled {};
+
+struct CoroPromiseBase
+{
+	using FCanceller = void(*)(void* pCtx);
+
+	enum class State : LONG
+	{
+		Initial,	// 无
+		HasNext,	// 同步任务，需要恢复到等待方
+		Completed,	// 异步任务，已完成
+		Cancelled,	// 提供可设置为取消标志的能力
+	};
+
+	LONG m_cRef{ 2 };// 协程引用计数，Task对象与协程帧各持有一份引用
+	State m_eState{ State::Initial };
+	std::coroutine_handle<> m_hCoroNext{};// 同步任务应恢复到
+	FCanceller m_pfnCanceller{};
+	void* m_pCtx{};
+	std::function<void()> m_fnOnCancel{};
+	CSrwLock m_Lk{};
+
+	constexpr auto initial_suspend() { return std::suspend_never{}; }
+	auto final_suspend() noexcept
+	{
+		struct Awaiter
+		{
+			CoroPromiseBase& Pro;
+
+			constexpr bool await_ready() const noexcept { return false; }
+			bool await_suspend(std::coroutine_handle<>) const noexcept
+			{
+				// 如果状态为HasNext，则协程已作为同步任务，即它的任务对象被其他协程co_await，
+				// 外界根本不需要知道它的完成状态，因此状态字段对外界没有必要
+				// 
+				// 如果状态为Initial，则协程作为异步任务，为了让外界具有查询其完成状态的能力，
+				// 状态字段设置为Completed
+				//
+				_InterlockedCompareExchange((LONG*)&Pro.m_eState,
+					(LONG)State::Completed, (LONG)State::Initial);
+				if (Pro.m_hCoroNext)// 恢复到等待方
+					Pro.m_hCoroNext.resume();
+				// 若减引用后为0，说明Task已析构，则返回false，协程结束并自动销毁；否则，
+				// Task仍保留一份引用，此时返回true，协程暂停在最终暂停点，等待Task析构时主动销毁
+				//
+				return Pro.DecRef() != 0;
+			}
+			constexpr void await_resume() const noexcept {}
+		};
+
+		return Awaiter{ *this };
+	}
+
+	void unhandled_exception() try
+	{
+		std::rethrow_exception(std::current_exception());
+	}
+	catch (CExceptionCoroCancelled)
+	{
+		// 提供co_await检测取消的能力，在await_resume中抛出CExceptionCoroCancelled即可退出协程
+		__iso_volatile_store32((int*)&m_eState, (int)State::Cancelled);
+	}
+	catch (...)
+	{
+		abort();
+	}
+
+	EckInline LONG IncRef() noexcept
+	{
+		return _InterlockedIncrement(&m_cRef);
+	}
+
+	EckInline LONG DecRef() noexcept
+	{
+		return _InterlockedDecrement(&m_cRef);
+	}
+
+	bool SyncResumeToIt(std::coroutine_handle<> hCoroNext_) noexcept
+	{
+		EckAssert(!m_hCoroNext && L"不能重复等待同步任务");
+		m_hCoroNext = hCoroNext_;
+		// 若原状态为Completed或Canceled，无需执行当前协程。。
+		// 原状态不可能为HasNext
+		return (State)_InterlockedCompareExchange((LONG*)&m_eState,
+			(LONG)State::HasNext, (LONG)State::Initial) == State::Initial;
+	}
+
+	EckInline BOOL IsCompleted() const noexcept
+	{
+		return (State)__iso_volatile_load32((const int*)&m_eState) == State::Completed;
+	}
+
+	EckInline BOOL TryCancel() noexcept
+	{
+		auto eOld = (State)_InterlockedCompareExchange((LONG*)&m_eState,
+			(LONG)State::Cancelled, (LONG)State::Initial);
+		EckAssert(eOld != State::Cancelled && L"协程已经被取消过");
+		if (eOld == State::Initial)
+			goto Success;
+		eOld = (State)_InterlockedCompareExchange((LONG*)&m_eState,
+			(LONG)State::Cancelled, (LONG)State::HasNext);
+		if (eOld == State::HasNext)
+			goto Success;
+		return FALSE;
+	Success:;
+		CSrwWriteGuard _{ m_Lk };
+		if (m_pfnCanceller)
+			m_pfnCanceller(m_pCtx);
+		if (m_fnOnCancel)
+		{
+			auto fn{ std::move(m_fnOnCancel) };
+			fn();
+		}
+		return TRUE;
+	}
+
+	EckInline BOOL IsCanceled() const noexcept
+	{
+		return (State)__iso_volatile_load32((const int*)&m_eState) == State::Cancelled;
+	}
+
+	EckInline void SetCanceller(FCanceller pfnCanceller, void* pCtx) noexcept
+	{
+		CSrwWriteGuard _{ m_Lk };
+		m_pfnCanceller = pfnCanceller;
+		m_pCtx = pCtx;
+	}
+
+	EckInline void SetOnCancel(const std::function<void()>& fnOnCancel) noexcept
+	{
+		CSrwWriteGuard _{ m_Lk };
+		m_fnOnCancel = fnOnCancel;
+	}
+
+	EckInline void SetOnCancel(std::function<void()>&& fnOnCancel) noexcept
+	{
+		CSrwWriteGuard _{ m_Lk };
+		m_fnOnCancel = std::move(fnOnCancel);
+	}
+};
+
+template<class T>
+struct CoroRetVal
+{
+private:
+	std::optional<T> m_RetVal;
+public:
+	void return_value(T&& Val) noexcept
+	{
+		m_RetVal.emplace(std::forward<T>(Val));
+	}
+
+	constexpr auto& GetOptional() const noexcept { return m_RetVal; }
+};
+
+template<>
+struct CoroRetVal<void>
+{
+	constexpr void return_void() const noexcept {}
+};
+
+template<class TRet = void>
+struct CoroTask
+{
+	struct promise_type;
+
+	std::coroutine_handle<promise_type> hCoroutine{};
+
+	struct promise_type : CoroPromiseBase, CoroRetVal<TRet>
+	{
+		CoroTask get_return_object() noexcept
+		{
+			return CoroTask{ std::coroutine_handle<promise_type>::from_promise(*this) };
+		}
+	};
+
+	auto operator co_await() const noexcept
+	{
+		struct Awaiter
+		{
+			promise_type& Pro;
+			bool await_ready() const noexcept { return Pro.IsCompleted(); }
+			bool await_suspend(std::coroutine_handle<promise_type> h) const noexcept
+			{
+				return Pro.SyncResumeToIt(h);
+			}
+			constexpr void await_resume() const noexcept {}
+		};
+		return Awaiter{ hCoroutine.promise() };
+	}
+
+	CoroTask() = default;
+	CoroTask(std::coroutine_handle<promise_type> h) noexcept : hCoroutine{ h } {}
+	CoroTask(const CoroTask& x)
+	{
+		if (&x != this)
+		{
+			if (hCoroutine = x.hCoroutine)
+				hCoroutine.promise().IncRef();
+		}
+	}
+
+	CoroTask(CoroTask&& x) noexcept
+	{
+		std::swap(hCoroutine, x.hCoroutine);
+	}
+
+	CoroTask& operator=(const CoroTask& x)
+	{
+		if (&x != this)
+		{
+			CoroTask t{ hCoroutine };
+			if (hCoroutine = x.hCoroutine)
+				hCoroutine.promise().IncRef();
+		}
+		return *this;
+	}
+
+	CoroTask& operator=(CoroTask&& x) noexcept
+	{
+		std::swap(hCoroutine, x.hCoroutine);
+		return *this;
+	}
+
+	~CoroTask()
+	{
+		if (hCoroutine && (hCoroutine.promise().DecRef() == 0))
+			hCoroutine.destroy();
+	}
+
+	// Forwarder
+
+	EckInline BOOL IsCompleted() const noexcept { return hCoroutine.promise().IsCompleted(); }
+	EckInline BOOL TryCancel() noexcept { return hCoroutine.promise().TryCancel(); }
+	EckInline BOOL IsCanceled() const noexcept { return hCoroutine.promise().IsCanceled(); }
+};
+
+struct CoroTaskFireAndForget
+{
+	struct promise_type
+	{
+		constexpr CoroTaskFireAndForget get_return_object() const noexcept { return {}; }
+		constexpr void return_void() const noexcept {}
+		void unhandled_exception() const noexcept { std::abort(); }
+		constexpr auto initial_suspend() const noexcept { return std::suspend_never{}; }
+		constexpr auto final_suspend() const noexcept { return std::suspend_never{}; }
+	};
+
+	constexpr auto operator co_await() const noexcept { return std::suspend_never{}; }
+};
+
+namespace Priv
+{
+	template<class T>
+		requires std::is_base_of_v<CoroPromiseBase, T>
+	auto ToCoroPromiseBaseHandle(std::coroutine_handle<T> h)
+	{
+		return std::coroutine_handle<CoroPromiseBase>::from_address(h.address());
+	}
+
+	struct CancellationTokenAwaiter
+	{
+		CoroPromiseBase* pPro{};
+		bool await_ready() const noexcept { return false; }
+
+		template<class T>
+		bool await_suspend(std::coroutine_handle<T> h) noexcept
+		{
+			pPro = &ToCoroPromiseBaseHandle(h).promise();
+			return false;
+		}
+
+		constexpr auto await_resume() const noexcept
+		{
+			struct Token
+			{
+				CoroPromiseBase* pPro;
+				EckInline BOOL IsCanceled() const noexcept { return pPro->IsCanceled(); }
+			};
+			return Token{ pPro };
+		}
+	};
+
+	struct TimerAwaiter
+	{
+		LONGLONG ms;
+		CTpTimer Timer;
+		CoroPromiseBase* pPro;
+
+		static void CALLBACK TimerProc(TP_CALLBACK_INSTANCE*, void* pCtx, TP_TIMER*)
+		{
+			std::coroutine_handle<>::from_address(pCtx).resume();
+		}
+
+		constexpr bool await_ready() const noexcept { return false; }
+
+		template<class T>
+		void await_suspend(std::coroutine_handle<T> h) noexcept
+		{
+			pPro = &Priv::ToCoroPromiseBaseHandle(h).promise();
+			pPro->SetCanceller([](void* pCtx)
+				{
+					const auto p = (TimerAwaiter*)pCtx;
+					p->Timer.SetTimer(0, 0);
+				}, this);
+			Timer.Create(TimerProc, h.address(), nullptr);
+			Timer.SetTimer(-ms * 10000ll, 0);
+		}
+
+		void await_resume() const
+		{
+			if (pPro->IsCanceled())
+				throw CExceptionCoroCancelled{};
+		}
+	};
+}
+
+EckInline auto CoroGetCancellationToken() { return Priv::CancellationTokenAwaiter{}; }
+
+EckInline auto CoroResumeBackground()
+{
+	struct Awaiter
+	{
+		constexpr bool await_ready() const noexcept { return false; }
+		void await_suspend(std::coroutine_handle<> h) const noexcept
+		{
+			TpSimpleTryPost([](TP_CALLBACK_INSTANCE*, void* pCtx) {
+				std::coroutine_handle<>::from_address(pCtx).resume();
+				}, h.address(), nullptr);
+		}
+		constexpr void await_resume() const noexcept {}
+	};
+	return Awaiter{};
+}
+
+/// <summary>
+/// 等待指定时间
+/// </summary>
+/// <param name="ms">正值为相对时间，负值为绝对时间</param>
+/// <returns>等待体</returns>
+EckInline auto CoroSleep(LONGLONG ms) { return Priv::TimerAwaiter{ ms }; }
+ECK_NAMESPACE_END
---
+++ eck/ThreadPool.h
@@ -0,0 +1,363 @@
+﻿/*
+* WinEzCtrlKit Library
+*
+* ThreadPool.h : NT线程池
+*
+* Copyright(C) 2024 QingKong
+*/
+#pragma once
+#include "ECK.h"
+
+ECK_NAMESPACE_BEGIN
+class CThreadPool
+{
+private:
+	TP_POOL* m_pPool{};
+public:
+	CThreadPool() = default;
+	CThreadPool(const CThreadPool&) = delete;
+	CThreadPool& operator=(const CThreadPool&) = delete;
+	constexpr CThreadPool(CThreadPool&& x) noexcept : m_pPool{ x.m_pPool }
+	{
+		x.m_pPool = nullptr;
+	}
+	constexpr CThreadPool& operator=(CThreadPool&& x) noexcept
+	{
+		std::swap(m_pPool, x.m_pPool);
+		return *this;
+	}
+
+	~CThreadPool() { Close(); }
+
+	EckInline NTSTATUS Create() { Close(); return TpAllocPool(&m_pPool, nullptr); }
+
+	EckInline void Close()
+	{
+		if (m_pPool)
+		{
+			TpReleasePool(m_pPool);
+			m_pPool = nullptr;
+		}
+	}
+
+	EckInline void SetMaxThreads(DWORD c) { TpSetPoolMaxThreads(m_pPool, c); }
+
+	EckInline NTSTATUS SetMinThreads(DWORD c) { return TpSetPoolMinThreads(m_pPool, c); }
+
+	EckInline constexpr [[nodiscard]] TP_POOL* GetPool() const noexcept { return m_pPool; }
+};
+
+class CTpEnvironment
+{
+private:
+	TP_CALLBACK_ENVIRON m_Env;
+public:
+	CTpEnvironment()
+	{
+		TpInitializeCallbackEnviron(&m_Env);
+	}
+	CTpEnvironment(const CTpEnvironment&) = delete;
+	CTpEnvironment& operator=(const CTpEnvironment&) = delete;
+	CTpEnvironment(CTpEnvironment&&) = delete;
+	CTpEnvironment& operator=(CTpEnvironment&&) = delete;
+	~CTpEnvironment()
+	{
+		TpDestroyCallbackEnviron(&m_Env);
+	}
+
+	EckInline constexpr auto& GetEnv() const noexcept { return m_Env; }
+	EckInline constexpr auto& GetEnv() noexcept { return m_Env; }
+
+	EckInline void SetCleanupGroup(TP_CLEANUP_GROUP* pGroup,
+		PTP_CLEANUP_GROUP_CANCEL_CALLBACK pfnCancel = nullptr)
+	{
+		TpSetCallbackCleanupGroup(&m_Env, pGroup, pfnCancel);
+	}
+
+	EckInline void SetLibrary(HMODULE hMod) { TpSetCallbackRaceWithDll(&m_Env, hMod); }
+
+	EckInline void SetPool(TP_POOL* Pool) { TpSetCallbackThreadpool(&m_Env, Pool); }
+	EckInline void SetPool(CThreadPool& Pool) { SetPool(Pool.GetPool()); }
+
+	EckInline void SetPriority(TP_CALLBACK_PRIORITY Priority) { TpSetCallbackPriority(&m_Env, Priority); }
+
+	EckInline void SetRunsLong(BOOL bRunsLong) { TpSetCallbackLongFunction(&m_Env); }
+};
+
+class CTpWork
+{
+private:
+	TP_WORK* m_pWork{};
+
+	template<class TTuple, size_t... Idx>
+	static void CALLBACK EckWorkCallback(PTP_CALLBACK_INSTANCE Instance, PVOID Context,
+		PTP_WORK Work)
+	{
+		std::unique_ptr<TTuple> pParam((TTuple*)(Context));
+		std::invoke(std::move(std::get<Idx>(*pParam))..., Instance, Work);
+	}
+
+	template<class TTuple, size_t... Idx>
+	static auto MakeCallback(std::index_sequence<Idx...>)
+	{
+		return &EckWorkCallback<TTuple, Idx...>;
+	}
+public:
+	CTpWork() = default;
+	CTpWork(const CTpWork&) = delete;
+	CTpWork& operator=(const CTpWork&) = delete;
+	constexpr CTpWork(CTpWork&& x) noexcept : m_pWork{ x.m_pWork }
+	{
+		x.m_pWork = nullptr;
+	}
+	constexpr CTpWork& operator=(CTpWork&& x) noexcept
+	{
+		std::swap(m_pWork, x.m_pWork);
+		return *this;
+	}
+
+	~CTpWork() { Close(); }
+
+	EckInline NTSTATUS Create(PTP_WORK_CALLBACK pfnCallback, void* pContext,
+		TP_CALLBACK_ENVIRON* pEnv = nullptr)
+	{
+		Close();
+		return TpAllocWork(&m_pWork, pfnCallback, pContext, pEnv);
+	}
+
+	EckInline NTSTATUS Create(PTP_WORK_CALLBACK pfnCallback, void* pContext,
+		CTpEnvironment& Env)
+	{
+		return Create(pfnCallback, pContext, &Env.GetEnv());
+	}
+
+	// F = void()(..., PTP_CALLBACK_INSTANCE Instance, PTP_WORK Work)
+	template<class F, class... TArgs>
+	EckInline NTSTATUS Create2(TP_CALLBACK_ENVIRON* pEnv, F&& Fn, TArgs&&... Args)
+	{
+		using TTuple = std::tuple<std::decay_t<F>, std::decay_t<TArgs>...>;
+		auto pParam = std::make_unique<TTuple>(std::forward<F>(Fn), std::forward<TArgs>(Args)...);
+		return Create(MakeCallback<TTuple>(std::make_index_sequence<sizeof...(TArgs) + 1>{}),
+			pParam.release(), pEnv);
+	}
+
+	EckInline void Close()
+	{
+		if (m_pWork)
+		{
+			TpReleaseWork(m_pWork);
+			m_pWork = nullptr;
+		}
+	}
+
+	EckInline void Submit() { TpPostWork(m_pWork); }
+
+	EckInline void Wait(BOOL bCancelPending = FALSE)
+	{
+		TpWaitForWork(m_pWork, (LOGICAL)bCancelPending);
+	}
+
+	EckInline constexpr [[nodiscard]] TP_WORK* GetWork() const noexcept { return m_pWork; }
+};
+
+class CTpWait
+{
+private:
+	TP_WAIT* m_pWait{};
+
+	template<class TTuple, size_t... Idx>
+	static void CALLBACK EckWaitCallback(PTP_CALLBACK_INSTANCE Instance, PVOID Context,
+		PTP_WAIT Wait, TP_WAIT_RESULT Result)
+	{
+		std::unique_ptr<TTuple> pParam((TTuple*)(Context));
+		std::invoke(std::move(std::get<Idx>(*pParam))..., Instance, Wait, Result);
+	}
+
+	template<class TTuple, size_t... Idx>
+	static auto MakeCallback(std::index_sequence<Idx...>)
+	{
+		return &EckWaitCallback<TTuple, Idx...>;
+	}
+public:
+	CTpWait() = default;
+	CTpWait(const CTpWait&) = delete;
+	CTpWait& operator=(const CTpWait&) = delete;
+	constexpr CTpWait(CTpWait&& x) noexcept : m_pWait{ x.m_pWait }
+	{
+		x.m_pWait = nullptr;
+	}
+	constexpr CTpWait& operator=(CTpWait&& x) noexcept
+	{
+		std::swap(m_pWait, x.m_pWait);
+		return *this;
+	}
+
+	~CTpWait() { Close(); }
+
+	EckInline NTSTATUS Create(PTP_WAIT_CALLBACK pfnCallback, void* pContext,
+		TP_CALLBACK_ENVIRON* pEnv = nullptr)
+	{
+		Close();
+		return TpAllocWait(&m_pWait, pfnCallback, pContext, pEnv);
+	}
+
+	EckInline NTSTATUS Create(PTP_WAIT_CALLBACK pfnCallback, void* pContext,
+		CTpEnvironment& Env)
+	{
+		return Create(pfnCallback, pContext, &Env.GetEnv());
+	}
+
+	// F = void()(..., PTP_CALLBACK_INSTANCE Instance, PTP_WAIT Wait, TP_WAIT_RESULT Result)
+	template<class F, class... TArgs>
+	EckInline NTSTATUS Create2(TP_CALLBACK_ENVIRON* pEnv, F&& Fn, TArgs&&... Args)
+	{
+		using TTuple = std::tuple<std::decay_t<F>, std::decay_t<TArgs>...>;
+		auto pParam = std::make_unique<TTuple>(std::forward<F>(Fn), std::forward<TArgs>(Args)...);
+		return Create(MakeCallback<TTuple>(std::make_index_sequence<sizeof...(TArgs) + 2>{}),
+			pParam.release(), pEnv);
+	}
+
+	EckInline void Close()
+	{
+		if (m_pWait)
+		{
+			TpReleaseWait(m_pWait);
+			m_pWait = nullptr;
+		}
+	}
+
+	EckInline void SetWait(HANDLE hObj, LONGLONG llTimeout)
+	{
+		TpSetWait(m_pWait, hObj, (LARGE_INTEGER*)&llTimeout);
+	}
+
+	EckInline void SetWait(HANDLE hObj, LARGE_INTEGER liTimeout)
+	{
+		TpSetWait(m_pWait, hObj, &liTimeout);
+	}
+
+	EckInline void SetWait(HANDLE hObj)
+	{
+		TpSetWait(m_pWait, hObj, nullptr);
+	}
+
+	EckInline void Wait(BOOL bCancelPending = FALSE)
+	{
+		TpWaitForWait(m_pWait, (LOGICAL)bCancelPending);
+	}
+
+	EckInline constexpr [[nodiscard]] TP_WAIT* GetWait() const noexcept { return m_pWait; }
+};
+
+class CTpTimer
+{
+private:
+	TP_TIMER* m_pTimer{};
+
+	template<class TTuple, size_t... Idx>
+	static void CALLBACK EckTimerCallback(PTP_CALLBACK_INSTANCE Instance, PVOID Context,
+		PTP_TIMER Timer)
+	{
+		std::unique_ptr<TTuple> pParam((TTuple*)(Context));
+		std::invoke(std::move(std::get<Idx>(*pParam))..., Instance, Timer);
+	}
+
+	template<class TTuple, size_t... Idx>
+	static auto MakeCallback(std::index_sequence<Idx...>)
+	{
+		return &EckTimerCallback<TTuple, Idx...>;
+	}
+public:
+	CTpTimer() = default;
+	CTpTimer(const CTpTimer&) = delete;
+	CTpTimer& operator=(const CTpTimer&) = delete;
+	constexpr CTpTimer(CTpTimer&& x) noexcept : m_pTimer{ x.m_pTimer }
+	{
+		x.m_pTimer = nullptr;
+	}
+	constexpr CTpTimer& operator=(CTpTimer&& x) noexcept
+	{
+		std::swap(m_pTimer, x.m_pTimer);
+		return *this;
+	}
+
+	~CTpTimer() { Close(); }
+
+	EckInline NTSTATUS Create(PTP_TIMER_CALLBACK pfnCallback, void* pContext,
+		TP_CALLBACK_ENVIRON* pEnv = nullptr)
+	{
+		Close();
+		return TpAllocTimer(&m_pTimer, pfnCallback, pContext, pEnv);
+	}
+
+	EckInline NTSTATUS Create(PTP_TIMER_CALLBACK pfnCallback, void* pContext,
+		CTpEnvironment& Env)
+	{
+		return Create(pfnCallback, pContext, &Env.GetEnv());
+	}
+
+	// F = void()(..., PTP_CALLBACK_INSTANCE Instance, PTP_TIMER Timer)
+	template<class F, class... TArgs>
+	EckInline NTSTATUS Create2(TP_CALLBACK_ENVIRON* pEnv, F&& Fn, TArgs&&... Args)
+	{
+		using TTuple = std::tuple<std::decay_t<F>, std::decay_t<TArgs>...>;
+		auto pParam = std::make_unique<TTuple>(std::forward<F>(Fn), std::forward<TArgs>(Args)...);
+		return Create(MakeCallback<TTuple>(std::make_index_sequence<sizeof...(TArgs) + 1>{}),
+			pParam.release(), pEnv);
+	}
+
+	EckInline void Close()
+	{
+		if (m_pTimer)
+		{
+			TpReleaseTimer(m_pTimer);
+			m_pTimer = nullptr;
+		}
+	}
+
+	EckInline void SetTimer2(LARGE_INTEGER* pliDueTime, DWORD msPeriod, DWORD msAdjust = 0)
+	{
+		TpSetTimer(m_pTimer, pliDueTime, msPeriod, msAdjust);
+	}
+
+	EckInline void SetTimer(LONGLONG llDueTime, DWORD msPeriod, DWORD msAdjust = 0)
+	{
+		SetTimer2((LARGE_INTEGER*)&llDueTime, msPeriod, msAdjust);
+	}
+
+	EckInline BOOL IsSet() const { return TpIsTimerSet(m_pTimer); }
+
+	EckInline void Wait(BOOL bCancelPending = FALSE)
+	{
+		TpWaitForTimer(m_pTimer, (LOGICAL)bCancelPending);
+	}
+
+	EckInline constexpr [[nodiscard]] TP_TIMER* GetTimer() const noexcept { return m_pTimer; }
+};
+
+namespace Priv
+{
+	template<class TTuple, size_t... Idx>
+	void CALLBACK EckTpSimpleCallback(PTP_CALLBACK_INSTANCE Instance, PVOID Context)
+	{
+		std::unique_ptr<TTuple> pParam((TTuple*)(Context));
+		std::invoke(std::move(std::get<Idx>(*pParam))..., Instance);
+	}
+
+	template<class TTuple, size_t... Idx>
+	auto MakeTpSimpleCallback(std::index_sequence<Idx...>)
+	{
+		return &EckTpSimpleCallback<TTuple, Idx...>;
+	}
+}
+
+// F = void()(..., PTP_CALLBACK_INSTANCE Instance)
+template<class F, class... TArgs>
+EckInline NTSTATUS TpSubmitSimpleCallback(TP_CALLBACK_ENVIRON* pEnv, F&& Fn, TArgs&&... Args)
+{
+	using TTuple = std::tuple<std::decay_t<F>, std::decay_t<TArgs>...>;
+	auto pParam = std::make_unique<TTuple>(std::forward<F>(Fn), std::forward<TArgs>(Args)...);
+	return TpSimpleTryPost(Priv::MakeTpSimpleCallback<TTuple>(std::make_index_sequence<sizeof...(TArgs)>{}),
+		pParam.release(), pEnv);
+}
+ECK_NAMESPACE_END
