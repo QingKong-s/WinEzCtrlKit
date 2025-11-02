6930b3fb596eac18b610aadf2031406c1dfe37e5
qk <85665984+QingKong-s@users.noreply.github.com>
2025-11-02 20:32:25 +0800

    增加NT对象基类，因此原先CFile的句柄生命周期管理已移除

---
+++ eck/CNtObject.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include "ECK.h"
+
+ECK_NAMESPACE_BEGIN
+class CNtObject
+{
+protected:
+    HANDLE m_hObject{};
+public:
+    CNtObject() = default;
+    CNtObject(HANDLE h) noexcept
+    {
+        NtDuplicateObject(NtCurrentProcess(), h,
+            NtCurrentProcess(), &m_hObject, 0, 0,
+            DUPLICATE_SAME_ACCESS | DUPLICATE_SAME_ATTRIBUTES);
+    }
+    CNtObject(CNtObject& x) noexcept : CNtObject{ x.Get() } {}
+    CNtObject(CNtObject&& x) noexcept { std::swap(m_hObject, x.m_hObject); }
+    CNtObject& operator=(CNtObject& x) noexcept
+    {
+        if (&x == this)
+            return *this;
+        NtDuplicateObject(NtCurrentProcess(), x.Get(),
+            NtCurrentProcess(), &m_hObject, 0, 0,
+            DUPLICATE_SAME_ACCESS | DUPLICATE_SAME_ATTRIBUTES);
+        return *this;
+    }
+    CNtObject& operator=(CNtObject&& x) noexcept { std::swap(m_hObject, x.m_hObject); }
+    ~CNtObject()
+    {
+        if (m_hObject && m_hObject != INVALID_HANDLE_VALUE)
+            NtClose(m_hObject);
+    }
+
+    EckInlineNdCe HANDLE Attach(HANDLE h) noexcept { return std::exchange(m_hObject, h); }
+    EckInlineNdCe HANDLE Detach() noexcept { return std::exchange(m_hObject, nullptr); }
+    EckInlineNdCe HANDLE Get() const noexcept { return m_hObject; }
+    EckInline void Clear() noexcept
+    {
+        if (m_hObject && m_hObject != INVALID_HANDLE_VALUE)
+        {
+            NtClose(m_hObject);
+            m_hObject = nullptr;
+        }
+    }
+    EckInlineNdCe BOOL IsValid() const noexcept { return m_hObject && m_hObject != INVALID_HANDLE_VALUE; }
+};
+ECK_NAMESPACE_END
--- eck/CWaitableTimer.h
+++ eck/CWaitableTimer.h
@@ -1,79 +1,73 @@
 ﻿#pragma once
 #include "CWaitableObject.h"
+#include "Utility.h"
 
 ECK_NAMESPACE_BEGIN
 class CWaitableTimer : public CWaitableObject
 {
 public:
-	CWaitableTimer()
-	{
-		OBJECT_ATTRIBUTES oa;
-		InitObjAttr(oa);
-		NtCreateTimer(&m_hObj, TIMER_ALL_ACCESS, &oa, SynchronizationTimer);
-	}
+    CWaitableTimer()
+    {
+        OBJECT_ATTRIBUTES oa;
+        InitObjAttr(oa);
+        NtCreateTimer(&m_hObject, TIMER_ALL_ACCESS, &oa, SynchronizationTimer);
+    }
 
-	CWaitableTimer(PCWSTR pszName, BOOL bManualReset = FALSE, BOOL bInheritHandle = FALSE)
-	{
-		UNICODE_STRING us;
-		if (pszName)
-			RtlInitUnicodeString(&us, pszName);
-		OBJECT_ATTRIBUTES oa;
-		InitObjAttr(oa, (pszName ? &us : nullptr), (bInheritHandle ? OBJ_INHERIT : 0));
-		NtCreateTimer(&m_hObj, TIMER_ALL_ACCESS, &oa,
-			(bManualReset ? NotificationTimer : SynchronizationTimer));
-	}
+    CWaitableTimer(std::wstring_view svName, BOOL bManualReset = FALSE, BOOL bInheritHandle = FALSE)
+    {
+        const auto us{ StringViewToNtString(svName) };
+        OBJECT_ATTRIBUTES oa;
+        InitObjAttr(oa, (svName.empty() ? nullptr : &us), (bInheritHandle ? OBJ_INHERIT : 0));
+        NtCreateTimer(&m_hObject, TIMER_ALL_ACCESS, &oa,
+            (bManualReset ? NotificationTimer : SynchronizationTimer));
+    }
 
-	EckInline NTSTATUS SetDueTime100ns(LONGLONG ll)
-	{
-		ll *= -1;
-		return NtSetTimer(m_hObj, (LARGE_INTEGER*)&ll,
-			nullptr, nullptr, FALSE, 0, nullptr);
-	}
+    EckInline NTSTATUS SetDueTime100ns(LONGLONG ll)
+    {
+        ll *= -1;
+        return NtSetTimer(m_hObject, (LARGE_INTEGER*)&ll,
+            nullptr, nullptr, FALSE, 0, nullptr);
+    }
 
-	EckInline NTSTATUS SetDueTimeAndPeriod100ns(LONGLONG ll, LONG lPeriod)
-	{
-		ll *= -1;
-		return NtSetTimer(m_hObj, (LARGE_INTEGER*)&ll,
-			nullptr, nullptr, FALSE, lPeriod, nullptr);
-	}
+    EckInline NTSTATUS SetDueTimeAndPeriod100ns(LONGLONG ll, LONG lPeriod)
+    {
+        ll *= -1;
+        return NtSetTimer(m_hObject, (LARGE_INTEGER*)&ll,
+            nullptr, nullptr, FALSE, lPeriod, nullptr);
+    }
 
-	EckInline NTSTATUS SetDueTime(LONG ll)
-	{
-		return SetDueTime100ns(ll * 10000ll);
-	}
+    EckInline NTSTATUS SetDueTime(LONG ll)
+    {
+        return SetDueTime100ns(ll * 10000ll);
+    }
 
-	EckInline NTSTATUS SetDueTimeAndPeriod(LONG ll, LONG lPeriod)
-	{
-		return SetDueTimeAndPeriod100ns(ll * 10000ll, lPeriod);
-	}
+    EckInline NTSTATUS SetDueTimeAndPeriod(LONG ll, LONG lPeriod)
+    {
+        return SetDueTimeAndPeriod100ns(ll * 10000ll, lPeriod);
+    }
 
-	EckInline NTSTATUS Cancel()
-	{
-		return NtCancelTimer(m_hObj, nullptr);
-	}
+    EckInline NTSTATUS Cancel(BOOLEAN* pbPrevState = nullptr)
+    {
+        return NtCancelTimer(m_hObject, pbPrevState);
+    }
 
-	EckInline NTSTATUS Cancel(BOOLEAN& bPrevState)
-	{
-		return NtCancelTimer(m_hObj, &bPrevState);
-	}
-
-	NTSTATUS QueryState(BOOLEAN& bState, LONGLONG& llRemainingTime)
-	{
-		TIMER_BASIC_INFORMATION	tbi;
-		ULONG cbRet;
-		const auto nts = NtQueryTimer(m_hObj, TimerBasicInformation,
-			&tbi, sizeof(tbi), &cbRet);
-		if (NT_SUCCESS(nts))
-		{
-			bState = tbi.TimerState;
-			llRemainingTime = tbi.RemainingTime.QuadPart;
-		}
-		else
-		{
-			bState = FALSE;
-			llRemainingTime = 0;
-		}
-		return nts;
-	}
+    NTSTATUS QueryState(_Out_ BOOLEAN& bState, _Out_ LONGLONG& llRemainingTime)
+    {
+        TIMER_BASIC_INFORMATION	tbi;
+        ULONG cbRet;
+        const auto nts = NtQueryTimer(m_hObject, TimerBasicInformation,
+            &tbi, sizeof(tbi), &cbRet);
+        if (NT_SUCCESS(nts))
+        {
+            bState = tbi.TimerState;
+            llRemainingTime = tbi.RemainingTime.QuadPart;
+        }
+        else
+        {
+            bState = FALSE;
+            llRemainingTime = 0;
+        }
+        return nts;
+    }
 };
 ECK_NAMESPACE_END
