88faa93bad18bb7ffbce5bf085a8dd8863dfa4d7
qk <85665984+QingKong-s@users.noreply.github.com>
2025-11-04 21:07:17 +0800

    CNtObject生命周期管理调整；增加控制台模板

---
+++ EckConsoleTemplate/Entry.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+
+#include "eck\Env.h"
+
+Cio::CReader<char> ConIn{};
+Cio::CWriter<char> ConOut{};
+Cio::CWriter<char> ConErr{};
+
+int wmain(int argc, WCHAR** argv)
+{
+    _CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    ConIn.CloneFrom(eck::NaGetStandardInput());
+    ConOut.CloneFrom(eck::NaGetStandardOutput());
+    ConErr.CloneFrom(eck::NaGetStandardError());
+
+    eck::INITPARAM ip{};
+    ip.uFlags = eck::EIF_NOINITD2D | eck::EIF_NOINITDWRITE |
+        eck::EIF_NOINITTHREAD | eck::EIF_NOINITWIC | eck::EIF_NODARKMODE;
+    DWORD dwErr;
+    const auto eInitRet = eck::Init(NtCurrentImageBaseHInst(), &ip, &dwErr);
+    if (eInitRet != eck::InitStatus::Ok)
+    {
+        EckDbgPrintFormatMessage(dwErr);
+        ConOut.PrintLine(L"Init failed: ",
+            (int)eInitRet, "(0x", Cio::IoInt{ dwErr,16,8 }, ")");
+        return 0;
+    }
+
+    ConOut.PrintLine(u8"请输入您的年龄：");
+    int Age;
+    ConIn.Scan(Age);
+    ConOut.PrintLine(u8"您的年龄是：", Age);
+
+    eck::UnInit();
+    return 0;
+}
---
+++ EckConsoleTemplate/pch.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#define ECK_OPT_NO_DARKMODE 1
+#include "eck\PchInc.h"
+#include "eck\ConsoleIo.h"
+
+using eck::PCVOID;
+using eck::PCBYTE;
+
+namespace Cio = eck::Cio;
+
+extern Cio::CReader<char> ConIn;
+extern Cio::CWriter<char> ConOut;
+extern Cio::CWriter<char> ConErr;
