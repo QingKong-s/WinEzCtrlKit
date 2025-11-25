ab761cd92f3833a8ef601ac2a3b0b408d5f236a1
QingKong <639582106@qq.com>
2025-11-25 16:36:33 +0800

    CColorReducing优化

---
+++ UnitTest/TsCColorReducing.cpp
@@ -0,0 +1,92 @@
+﻿#include "pch.h"
+#include "../eck/CColorReducing.h"
+#include "../eck/ComPtr.h"
+
+#pragma comment(lib, "ntdll.lib")
+
+using eck::ComPtr;
+
+TS_NS_BEGIN
+TEST_CLASS(TsCColorReducing)
+{
+public:
+    TEST_METHOD(Ts)
+    {
+        return;
+        HRESULT hr;
+        (void)CoInitialize(nullptr);
+        ComPtr<IWICImagingFactory> pFactory;
+        pFactory.CreateInstance(CLSID_WICImagingFactory);
+
+        ComPtr<IWICBitmapDecoder> pDecoder;
+        hr = pFactory->CreateDecoderFromFilename(
+            LR"(E:\Desktop\Temp\1.jpg)",
+            nullptr, GENERIC_READ,
+            WICDecodeMetadataCacheOnLoad, &pDecoder);
+        if (FAILED(hr))
+            return;
+        ComPtr<IWICBitmapFrameDecode> pFrame;
+        pDecoder->GetFrame(0, &pFrame);
+        UINT w, h;
+        pFrame->GetSize(&w, &h);
+
+        ComPtr<IWICFormatConverter> pCvtTo32;
+        pFactory->CreateFormatConverter(&pCvtTo32);
+        pCvtTo32->Initialize(
+            pFrame.Get(),
+            GUID_WICPixelFormat32bppRGB,
+            WICBitmapDitherTypeNone,
+            nullptr, 0.0, WICBitmapPaletteTypeCustom);
+
+        std::vector<ARGB> vPixels(w * h);
+        pCvtTo32->CopyPixels(nullptr, w * 4, w * h * 4, (BYTE*)vPixels.data());
+
+        eck::CColorReducing Cr{ 256 };
+        for (const auto cr : vPixels)
+            Cr.AddColor(GetRValue(cr), GetGValue(cr), GetBValue(cr));
+
+        std::vector<ARGB> vPal(256);
+        Cr.GetPalette((UINT*)vPal.data(), 256);
+        for (auto& cr : vPal)
+            cr |= 0xFF00'0000;
+
+        ComPtr<IWICPalette> pPalette;
+        pFactory->CreatePalette(&pPalette);
+        pPalette->InitializeCustom((WICColor*)vPal.data(), 256);
+        //pPalette->InitializeFromBitmap(pFrame.Get(), 256, FALSE);
+
+
+        ComPtr<IWICBitmapEncoder> pEncoder;
+        hr = pFactory->CreateEncoder(GUID_ContainerFormatPng, nullptr, &pEncoder);
+        if (FAILED(hr))
+            return;
+
+        ComPtr<IWICStream> pStream;
+        pFactory->CreateStream(&pStream);
+        hr = pStream->InitializeFromFilename(
+            LR"(E:\Desktop\TsCColorReducing.png)", GENERIC_WRITE);
+
+        hr = pEncoder->Initialize(pStream.Get(), WICBitmapEncoderNoCache);
+        ComPtr<IWICBitmapFrameEncode> pFrameEncode;
+        hr = pEncoder->CreateNewFrame(&pFrameEncode, nullptr);
+
+        hr = pFrameEncode->Initialize(nullptr);
+        hr = pFrameEncode->SetSize(w, h);
+        auto Fmt = GUID_WICPixelFormat8bppIndexed;
+        hr = pFrameEncode->SetPixelFormat(&Fmt);
+        hr = pFrameEncode->SetPalette(pPalette.Get());
+
+        ComPtr<IWICFormatConverter> pCvtTo8;
+        hr = pFactory->CreateFormatConverter(&pCvtTo8);
+        hr = pCvtTo8->Initialize(
+            pFrame.Get(),
+            GUID_WICPixelFormat8bppIndexed,
+            WICBitmapDitherTypeNone,
+            pPalette.Get(), 0.0, WICBitmapPaletteTypeCustom);
+
+        hr = pFrameEncode->WriteSource(pCvtTo8.Get(), nullptr);
+        hr = pFrameEncode->Commit();
+        hr = pEncoder->Commit();
+    }
+};
+TS_NS_END
--- eck/CColorReducing.h
+++ eck/CColorReducing.h
@@ -5,217 +5,227 @@ ECK_NAMESPACE_BEGIN
 class CColorReducing
 {
 private:
-	struct NODE
-	{
-		UINT r, g, b;	// 颜色和
-		BOOLEAN bLeaf;	// 叶节点
-		BOOLEAN bCut;	// 是否可裁剪
-		BYTE cChild;	// 子节点数
-		UINT cColor;	// 颜色数
-		NODE* pChild[8];// 子节点
-		NODE* pNext;	// 同一层的后继节点，仅供常规节点使用
-	};
+    struct NODE
+    {
+        UINT r, g, b;       // 颜色和
+        UINT cChild : 4;    // 子节点数
+        UINT bLeaf : 1;     // 叶节点
+        UINT bCut : 1;      // 是否可裁剪
+        UINT cColor : 26;   // 颜色数
+        NODE* pChild[8];    // 子节点
+        NODE* pNext;        // 同一层的后继节点，仅供常规节点使用
+    };
 
-	CMemorySet m_MemSet{};
-	NODE m_Root{};
-	std::array<NODE*, 8> m_apLast{ &m_Root };
-	UINT m_cValid;		// 不重复的颜色数
-	UINT m_cMax;		// 最大颜色数
-	BOOL m_bNotInit;	// 是否非首次运行
+    CMemorySet m_MemSet{};
+    NODE m_Root{};
+    NODE* m_apLast[8]{ &m_Root };
+    UINT m_cValid;      // 不重复的颜色数
+    UINT m_cMax;        // 最大颜色数
+    BOOL m_bNotInit;    // 是否非首次运行
 
-	inline UINT ComputeColorError(NODE* p)
-	{
-		UINT avgR = p->r / p->cColor;
-		UINT avgG = p->g / p->cColor;
-		UINT avgB = p->b / p->cColor;
-		UINT error = 0;
-		for (auto& child : p->pChild)
-		{
-			if (child)
-			{
-				error += (child->r - avgR) * (child->r - avgR);
-				error += (child->g - avgG) * (child->g - avgG);
-				error += (child->b - avgB) * (child->b - avgB);
-			}
-		}
-		return error;
-	}
+    constexpr UINT ComputeColorError(NODE* p) noexcept
+    {
+        if (!p->cChild)
+            return 0;
+        const auto AvgR = p->r / p->cColor;
+        const auto AvgG = p->g / p->cColor;
+        const auto AvgB = p->b / p->cColor;
+        UINT t{};
+        for (auto& e : p->pChild)
+        {
+            if (!e)
+                continue;
+            const auto ChildAvgR = e->r / e->cColor;
+            const auto ChildAvgG = e->g / e->cColor;
+            const auto ChildAvgB = e->b / e->cColor;
 
-	void RemoveChild(NODE* p)
-	{
-		if (!p->bCut)
-			return;
-		p->bCut = FALSE;
-		for (auto e : p->pChild)
-		{
-			if (e)
-				RemoveChild(e);
-		}
-	}
+            const auto dr = (ChildAvgR > AvgR) ? (ChildAvgR - AvgR) : (AvgR - ChildAvgR);
+            const auto dg = (ChildAvgG > AvgG) ? (ChildAvgG - AvgG) : (AvgG - ChildAvgG);
+            const auto db = (ChildAvgB > AvgB) ? (ChildAvgB - AvgB) : (AvgB - ChildAvgB);
 
-	void ReduceColor()
-	{
-		NODE* pCurr{};
-		UINT cMin{ 0xFFFFFFFF };
+            t += (dr * dr + dg * dg + db * db) * e->cColor;
+        }
+        return t;
+    }
 
-		for (int i = 7; i >= 0; --i)
-		{
-			auto p{ m_apLast[i] };
-			while (p)
-			{
-				if (p->cChild && p->bCut && p->cColor < cMin)
-				{
-					cMin = p->cColor;
-					pCurr = p;
-					if (cMin == 1)
-						break;
-				}
+    constexpr void RemoveChild(NODE* p) noexcept
+    {
+        if (!p->bCut)
+            return;
+        p->bCut = FALSE;
+        for (auto e : p->pChild)
+        {
+            if (e)
+                RemoveChild(e);
+        }
+    }
 
-				if (!(p = p->pNext))
-					break;
-			}
-			if (pCurr)
-				break;
-			else
-				m_apLast[i] = nullptr;
-		}
-		EckAssert(pCurr);
-		pCurr->bLeaf = TRUE;
-		m_cValid -= (pCurr->cChild - 1);
-		RemoveChild(pCurr);
-	}
+    constexpr void ReduceColor() noexcept
+    {
+        NODE* pCurr{};
+        UINT MinError{ 0xFFFFFFFF };
+        // 从最深层开始搜索
+        for (int i = 7; i >= 0; --i)
+        {
+            auto p{ m_apLast[i] };
+            while (p)
+            {
+                if (p->cChild && p->bCut)
+                {
+                    UINT error = ComputeColorError(p);
+                    // 选择误差最小的节点进行合并
+                    if (error < MinError)
+                    {
+                        MinError = error;
+                        pCurr = p;
+                    }
+                }
+                p = p->pNext;
+            }
+            if (pCurr)
+                break;
+            else
+                m_apLast[i] = nullptr;
+        }
+        EckAssert(pCurr);
+        pCurr->bLeaf = TRUE;
+        m_cValid -= (pCurr->cChild - 1);
+        RemoveChild(pCurr);
+    }
 public:
-	CColorReducing()
-	{
-		m_MemSet.SetPageSize(4096 * 10);
-		m_Root.bCut = TRUE;
-	}
-	CColorReducing(UINT cMaxColor) : CColorReducing{}
-	{
-		m_cMax = cMaxColor;
-	}
+    CColorReducing() noexcept
+    {
+        m_MemSet.SetPageSize(4096 * 30);
+        m_Root.bCut = TRUE;
+    }
+    CColorReducing(UINT cMaxColor) noexcept : CColorReducing{}
+    {
+        m_cMax = cMaxColor;
+    }
 
-	void Reset()
-	{
-		m_MemSet.ClearRecord();
-		m_Root = {};
-		m_bNotInit = TRUE;
-		m_Root.bCut = TRUE;
-		m_apLast = {};
-	}
+    // 最大颜色数不会被重置
+    void Reset() noexcept
+    {
+        m_MemSet.ClearRecord();
+        m_Root = {};
+        m_bNotInit = TRUE;
+        m_Root.bCut = TRUE;
+        ZeroMemory(m_apLast, sizeof(m_apLast));
+    }
 
-	void AddColor(UINT r, UINT g, UINT b)
-	{
-		UINT nCurrLevel{}, nShift{ 7 }, idx;
-		NODE* pCurrNode{ &m_Root };
-		pCurrNode->r += r;
-		pCurrNode->g += g;
-		pCurrNode->b += b;
-		pCurrNode->cColor += 1;
-		for (; nCurrLevel < 8; ++nCurrLevel, --nShift)
-		{
-			idx = (((r >> nShift) << 2) & 0b100) |
-				(((g >> nShift) << 1) & 0b010) |
-				((b >> nShift) & 0b001);
-			if (!pCurrNode->pChild[idx])
-			{
-				const auto p = (NODE*)m_MemSet.Allocate(sizeof(NODE));
-				pCurrNode->pChild[idx] = p;
-				if (!m_bNotInit)
-					ZeroMemory(p, sizeof(NODE));
-				if (nCurrLevel == 7)
-				{
-					p->bLeaf = TRUE;
-					++m_cValid;
-					while (m_cValid > m_cMax)
-						ReduceColor();
-				}
-				else
-				{
-					p->pNext = m_apLast[nCurrLevel + 1];
-					m_apLast[nCurrLevel + 1] = p;
-				}
-				p->bCut = TRUE;
+    void AddColor(UINT r, UINT g, UINT b) noexcept
+    {
+        UINT nCurrLevel{}, nShift{ 7 }, idx;
+        NODE* pCurrNode{ &m_Root };
+        pCurrNode->r += r;
+        pCurrNode->g += g;
+        pCurrNode->b += b;
+        pCurrNode->cColor += 1;
+        for (; nCurrLevel < 8; ++nCurrLevel, --nShift)
+        {
+            idx = (((r >> nShift) << 2) & 0b100) |
+                (((g >> nShift) << 1) & 0b010) |
+                ((b >> nShift) & 0b001);
+            if (!pCurrNode->pChild[idx])
+            {
+                const auto p = (NODE*)m_MemSet.Allocate(sizeof(NODE));
+                pCurrNode->pChild[idx] = p;
+                if (!m_bNotInit)
+                    ZeroMemory(p, sizeof(NODE));
+                if (nCurrLevel == 7)
+                {
+                    p->bLeaf = TRUE;
+                    ++m_cValid;
+                    while (m_cValid > m_cMax)
+                        ReduceColor();
+                }
+                else
+                {
+                    p->pNext = m_apLast[nCurrLevel + 1];
+                    m_apLast[nCurrLevel + 1] = p;
+                }
+                p->bCut = TRUE;
 
-				++pCurrNode->cChild;
-			}
-			pCurrNode = pCurrNode->pChild[idx];
-			pCurrNode->r += r;
-			pCurrNode->g += g;
-			pCurrNode->b += b;
-			pCurrNode->cColor += 1;
-			if (pCurrNode->bLeaf)
-				break;
-		}
-	}
+                ++pCurrNode->cChild;
+            }
+            pCurrNode = pCurrNode->pChild[idx];
+            pCurrNode->r += r;
+            pCurrNode->g += g;
+            pCurrNode->b += b;
+            pCurrNode->cColor += 1;
+            if (pCurrNode->bLeaf)
+                break;
+        }
+    }
 
-	BOOL GetNearestColor(UINT r, UINT g, UINT b,
-		UINT& rOut, UINT& gOut, UINT& bOut)
-	{
-		UINT nCurrLevel{}, nShift{ 7 }, idx;
-		NODE* p{ &m_Root };
-		if (p->bLeaf)
-		{
-			rOut = p->r / p->cColor;
-			gOut = p->g / p->cColor;
-			bOut = p->b / p->cColor;
-			return TRUE;
-		}
-		for (; nCurrLevel < 8; ++nCurrLevel, --nShift)
-		{
-			idx = (((r >> nShift) << 2) & 0b100) |
-				(((g >> nShift) << 1) & 0b010) |
-				((b >> nShift) & 0b001);
-			p = p->pChild[idx];
-			if (!p)
-				break;
-			if (p->bLeaf)
-			{
-				rOut = p->r / p->cColor;
-				gOut = p->g / p->cColor;
-				bOut = p->b / p->cColor;
-				return TRUE;
-			}
-		}
-		rOut = gOut = bOut = 0;
-		return FALSE;
-	}
+    BOOL GetNearestColor(UINT r, UINT g, UINT b,
+        _Out_ UINT& rOut, _Out_ UINT& gOut, _Out_ UINT& bOut) noexcept
+    {
+        UINT nCurrLevel{}, nShift{ 7 }, idx;
+        NODE* p{ &m_Root };
+        if (p->bLeaf)
+        {
+            rOut = p->r / p->cColor;
+            gOut = p->g / p->cColor;
+            bOut = p->b / p->cColor;
+            return TRUE;
+        }
+        for (; nCurrLevel < 8; ++nCurrLevel, --nShift)
+        {
+            idx = (((r >> nShift) << 2) & 0b100) |
+                (((g >> nShift) << 1) & 0b010) |
+                ((b >> nShift) & 0b001);
+            p = p->pChild[idx];
+            if (!p)
+                break;
+            if (p->bLeaf)
+            {
+                rOut = p->r / p->cColor;
+                gOut = p->g / p->cColor;
+                bOut = p->b / p->cColor;
+                return TRUE;
+            }
+        }
+        rOut = gOut = bOut = 0;
+        return FALSE;
+    }
 
-	UINT GetPalette(_Out_writes_(cMaxColor) UINT* pColors, UINT cMaxColor)
-	{
-		std::vector<NODE*> s{};
-		s.reserve(16);
-		s.push_back(&m_Root);
-		auto pcr{ pColors };
-		while (!s.empty())
-		{
-			auto p = s.back();
-			s.pop_back();
-			if (p->bLeaf)
-			{
-				*pcr++ = ((BYTE(p->r / p->cColor) << 16) |
-					(BYTE(p->g / p->cColor) << 8) |
-					BYTE(p->b / p->cColor));
-				if (p->cColor == m_cMax)
-					break;
-			}
-			else
-			{
-				for (auto e : p->pChild)
-				{
-					if (e)
-						s.push_back(e);
-				}
-			}
-		}
-		return UINT(pcr - pColors);
-	}
+    // 返回实际颜色数
+    UINT GetPalette(_Out_writes_(cMaxColor) UINT* pColors, UINT cMaxColor) noexcept
+    {
+        std::vector<NODE*> s{};
+        s.reserve(256);
+        s.push_back(&m_Root);
+        UINT c = 0;
+        while (!s.empty() && c < cMaxColor)
+        {
+            const auto p = s.back();
+            s.pop_back();
+            if (p->bLeaf)
+            {
+                *pColors++ = ((BYTE(p->r / p->cColor) << 16) |
+                    (BYTE(p->g / p->cColor) << 8) |
+                    BYTE(p->b / p->cColor));
+                ++c;
+            }
+            else
+            {
+                for (auto e : p->pChild)
+                {
+                    if (e)
+                        s.push_back(e);
+                }
+            }
+        }
+        return c;
+    }
 
-	UINT Reduce()
-	{
-		while (m_cValid > m_cMax)
-			ReduceColor();
-		return m_cValid;
-	}
+    // 减色直到颜色数少于设定的最大值，返回实际颜色数
+    UINT Reduce() noexcept
+    {
+        while (m_cValid > m_cMax)
+            ReduceColor();
+        return m_cValid;
+    }
 };
 ECK_NAMESPACE_END
